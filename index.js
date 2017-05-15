3cb1ae4ca046d373075e0420da79c3c163330d7f
dominguezcelada <dominguez.celada@gmail.com>
2017-05-15 11:25:44 +0200

    Integrating fotocasa. In progress

--- index.js
+++ index.js
@@ -3,59 +3,77 @@ var page = require('webpage').create();
 var system = require('system');
 var args = system.args;
 
-var ZONE_LES_CORTS = "https://www.idealista.com/alquiler-viviendas/barcelona/les-corts/les-corts/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,publicado_ultimas-24-horas/";
-var ZONE_EIXAMPLE = "https://www.idealista.com/alquiler-viviendas/barcelona/eixample/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,publicado_ultimas-24-horas/";
-var ZONE_CIUTAT_VELLA = "https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,publicado_ultimas-24-horas/";
-var ZONE_GRACIA = "https://www.idealista.com/alquiler-viviendas/barcelona/gracia/vila-de-gracia/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,amueblado_amueblados,publicado_ultimas-24-horas/";
-var ZONE_SANTS = "https://www.idealista.com/alquiler-viviendas/barcelona/sants-montjuic/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,amueblado_amueblados,publicado_ultimas-24-horas/";
+var URLS_IDEALISTA = {
+    ZONE_CIUTAT_VELLA: "https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,publicado_ultimas-24-horas/",
+    ZONE_EIXAMPLE: "https://www.idealista.com/alquiler-viviendas/barcelona/eixample/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,publicado_ultimas-24-horas/",
+    ZONE_GRACIA: "https://www.idealista.com/alquiler-viviendas/barcelona/gracia/vila-de-gracia/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,amueblado_amueblados,publicado_ultimas-24-horas/",
+    ZONE_LES_CORTS: "https://www.idealista.com/alquiler-viviendas/barcelona/les-corts/les-corts/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,publicado_ultimas-24-horas/",
+    ZONE_SANTS: "https://www.idealista.com/alquiler-viviendas/barcelona/sants-montjuic/con-precio-hasta_1300,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,amueblado_amueblados,publicado_ultimas-24-horas/",
+};
 
-var ZONES_IDEALISTA = [
-    { 
-        name: "LES CORTS", 
-        url: ZONE_LES_CORTS,
-        param: "--les-corts",
-    },
-    { 
-        name: "EIXAMPLE", 
-        url: ZONE_EIXAMPLE,
-        param: "--eixample",
-    },
+var URLS_FOTOCASA = {
+    ZONE_CIUTAT_VELLA: "http://www.fotocasa.es/es/alquiler/casas/barcelona-capital/ciutat-vella/amueblado/l?latitude=41.3854&longitude=2.1775&maxPrice=1250&minRooms=2&combinedLocationIds=724,9,8,232,376,8019,0,1152,0",
+    ZONE_EIXAMPLE: "http://www.fotocasa.es/es/alquiler/casas/barcelona-capital/eixample/amueblado/l?latitude=41.3854&longitude=2.1775&maxPrice=1250&minRooms=2&combinedLocationIds=724,9,8,232,376,8019,0,1151,0",
+    ZONE_GRACIA: "http://www.fotocasa.es/es/alquiler/casas/barcelona-capital/vila-de-gracia/amueblado/l?latitude=41.3854&longitude=2.1775&maxPrice=1250&minRooms=2&combinedLocationIds=724,9,8,232,376,8019,0,1150,342",
+    ZONE_LESCORTS: "http://www.fotocasa.es/es/alquiler/casas/barcelona-capital/les-corts/amueblado/l?latitude=41.3854&longitude=2.1775&maxPrice=1250&minRooms=2&combinedLocationIds=724,9,8,232,376,8019,0,1148,0",
+    ZONE_SANTS: "http://www.fotocasa.es/es/alquiler/casas/barcelona-capital/sants-montjuic/amueblado/l?latitude=41.3854&longitude=2.1775&maxPrice=1250&minRooms=2&combinedLocationIds=724,9,8,232,376,8019,0,1144,0",
+};
+
+var ZONE_URLS = args.indexOf("--fotocasa") >= 0? [].concat(URLS_FOTOCASA) : [].concat(URLS_IDEALISTA);
+
+var ZONES = [
     { 
         name: "CIUTAT VELLA", 
-        url: ZONE_CIUTAT_VELLA,
+        url: ZONE_URLS.ZONE_CIUTAT_VELLA,
         param: "--ciutat-vella",
     },
     { 
+        name: "EIXAMPLE", 
+        url: ZONE_URLS.ZONE_EIXAMPLE,
+        param: "--eixample",
+    },
+    { 
         name: "GRACIA", 
-        url: ZONE_GRACIA,
+        url: ZONE_URLS.ZONE_GRACIA,
         param: "--gracia",
     },
     { 
+        name: "LES CORTS", 
+        url: ZONE_URLS.ZONE_LES_CORTS,
+        param: "--les-corts",
+    },
+    { 
         name: "SANTS", 
-        url: ZONE_SANTS,
+        url: ZONE_URLS.ZONE_SANTS,
         param: "--sants",
     }
 ];
 
 if(args.length >= 2) {
-    ZONES_IDEALISTA = ZONES_IDEALISTA.filter(function(zone) {
+    ZONES = ZONES.filter(function(zone) {
         return args.indexOf(zone.param) >= 0;
     })
 }
 
 var numOpenedPages = 0;
 
-page.open(ZONES_IDEALISTA[numOpenedPages].url, scrapPage);
+
+
+// if(args.indexOf("--fotocasa") >= 0) {
+//     page.open(FOTOCASA_CIUTAT_VELLA, scrapPageFotoCasa);
+// } else {
+page.open(ZONES[numOpenedPages].url, scrapPage);
+// }
 
 function scrapPage(status) {
     console.log("\n-------------------------------------------------------------------");
-    console.log("              " + ZONES_IDEALISTA[numOpenedPages].name + "                  ");
+    console.log("              " + ZONES[numOpenedPages].name + "                  ");
     console.log("-------------------------------------------------------------------\n");
-    // console.log("****Status: " + status + "****\n");
+    console.log("****Status: " + status + "****\n");
     if(status === "success") {
-        // page.render('idealista.png');
         var recentApartments = page.evaluate(function() {
             var items = document.querySelectorAll(".item");
+            console.log(document);
             var recentApartments = [];
             for(var i = 0; i < items.length; i++) {
                 var apartment = items[i].querySelector(".item-link");
@@ -74,7 +92,7 @@ function scrapPage(status) {
         });
 
         recentApartments.forEach(function(recentApartment) {
-            if(recentApartment.time !== '') {
+            if(recentApartment.time !== '' && recentApartment.time.indexOf("día") < 0) {
                 console.log(recentApartment.title);
                 console.log(recentApartment.price);
                 console.log(recentApartment.time);
@@ -84,11 +102,58 @@ function scrapPage(status) {
         })
 
         numOpenedPages++;
-        if(numOpenedPages === ZONES_IDEALISTA.length) phantom.exit(0);
-        else {
-            page.open(ZONES_IDEALISTA[numOpenedPages].url, scrapPage);
+        if(numOpenedPages === ZONES.length) {
+            phantom.exit(0);
+        } else {
+            page.open(ZONES[numOpenedPages].url, scrapPage);
         }
     }
 }
+
+/*
+document.querySelectorAll(".re-Searchresult-itemRow")
+items[i].querySelector(".re-Card-title")
+items[i].querySelector(".re-Card-priceComposite")
+items[i].querySelector(".re-Card-timeago")
+items[i].querySelector(".re-Card-title")
+*/
+
+// function scrapPageFotoCasa(status) {
+//     console.log("\n-------------------------------------------------------------------");
+//     console.log("              FOTOCASA                  ");
+//     console.log("-------------------------------------------------------------------\n");
+//     // console.log("****Status: " + status + "****\n");
+//     if(status === "success") {
+//         var recentApartments = page.evaluate(function() {
+//             var items = document.querySelectorAll(".re-Searchresult-itemRow");
+//             var recentApartments = [];
+//             for(var i = 0; i < items.length; i++) {
+//                 var apartment = items[i].querySelector(".re-Card-title");
+//                 var price = items[i].querySelector(".re-Card-priceComposite");
+//                 var redText = items[i].querySelector(".re-Card-timeago");
+//                 var url = items[i].querySelector(".re-Card-title");
+                
+//                 recentApartments.push({
+//                     title: apartment && apartment.textContent,
+//                     price: price && price.textContent,
+//                     time: redText && redText.textContent,
+//                     url: apartment && apartment.href
+//                 });
+//             }
+//             return recentApartments;
+//         });
+
+//         recentApartments.forEach(function(recentApartment) {
+//             if(recentApartment.time.indexOf("día") < 0) {
+//                 console.log(recentApartment.title);
+//                 console.log(recentApartment.price);
+//                 console.log(recentApartment.time);
+//                 console.log(recentApartment.url);
+//                 console.log("\n");
+//             }
+//         })
+//         phantom.exit(0);
+//     }
+// }
 
 
